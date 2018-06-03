c822e2ee63ec9228f378d9f9535dddae9e0fe2eb
Fahmikt <36624890+Fahmikt@users.noreply.github.com>
2018-06-03 15:34:39 +0700

    Update safelink_utama.js

--- safelink_utama.js
+++ safelink_utama.js
@@ -24,8 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="sambelin-4.html"}
 if(x=="2"){xxx="sambelin-3.html"}
 if(x=="3"){xxx="on-off-chance-that-you-are-blogger-and.html"}
-if(x=="4"){xxx="the-most-effective-method-to-increase.html
-"}a_to_ve[i].href="https://www.sambelin.xyz/2018/05/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="the-most-effective-method-to-increase.html"}a_to_ve[i].href="https://www.sambelin.xyz/2018/05/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
